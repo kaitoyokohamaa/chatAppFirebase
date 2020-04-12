e96392472635b174b1fe32674b378b26d93db9de
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-04-12 14:10:30 +0900

    デプロイ実験

---
+++ build/precache-manifest.8af1c42b94a6329422bd6f4f4f283dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d241705afbf4e7f644e327f556514f0",
+    "url": "/chatAppFirebase/index.html"
+  },
+  {
+    "revision": "7c9c51a602c377aa0dfd",
+    "url": "/chatAppFirebase/static/css/main.80846999.chunk.css"
+  },
+  {
+    "revision": "4a705a8ce9288353a146",
+    "url": "/chatAppFirebase/static/js/2.fc3c1669.chunk.js"
+  },
+  {
+    "revision": "b9fec8e6bf3d0da287e58f49182fcab2",
+    "url": "/chatAppFirebase/static/js/2.fc3c1669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9c51a602c377aa0dfd",
+    "url": "/chatAppFirebase/static/js/main.cceb6dc4.chunk.js"
+  },
+  {
+    "revision": "671ddb09829e9251d959",
+    "url": "/chatAppFirebase/static/js/runtime-main.908c7953.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a678ec6a02553d0c9adcf554ef7609b.js"
+  "/chatAppFirebase/precache-manifest.8af1c42b94a6329422bd6f4f4f283dc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chatAppFirebase/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
